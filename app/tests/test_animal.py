78568d8cc580ba59894c96bc0f54dc0b6457045f
tomson <tomsonpai@163.com>
2020-10-26 16:06:08 +0800

    修改测试例子

--- app/tests/test_animal.py
+++ app/tests/test_animal.py
@@ -1,3 +1,9 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
 def test_animal(client):
     resp = client.get("/animals")
     assert resp.status_code == 200, resp.text
